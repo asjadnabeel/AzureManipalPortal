b01fd8a91d90d2bced8025773272f1d40d54e197
Asjad Nabeel <asjadnabeel@gmail.com>
2017-06-01 14:50:59 +0530

    ThirdCommit

--- amsler/settings.py
+++ amsler/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '79570b62-4abc-4b93-8d39-9559a48a281f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://manipalamslerportal.azurewebsites.net/' , '*']
 
 
 # Application definition
